2be549f718038bc80187edb68ae119ffc892ac82
Omar Ezz <oomarezzeldin7@gmail.com>
2023-01-13 16:39:00 +0200

    pyramid class

--- src/giu/santorini/tiles/Pyramid.java
+++ src/giu/santorini/tiles/Pyramid.java
@@ -1,7 +1,26 @@
 package giu.santorini.tiles;
+import java.util.ArrayList;
+
 import giu.santorini.utilities.Location;
 public class Pyramid extends Piece{
 	public Pyramid(Location Location){
 		super(Location);
 	}
+	public ArrayList<Location> possibleMoves(){
+		ArrayList<Location> m = new ArrayList<Location>();
+		if((this.Location.y+1>-1 & this.Location.y+1<6)&
+		(this.Location.x-1>-1 & this.Location.x-1<6))
+			m.add(new Location(this.Location.y+1,this.Location.x-1));
+		if((this.Location.y-1>-1 & this.Location.y-1<6)&
+				(this.Location.x-1>-1 & this.Location.x-1<6))
+			m.add(new Location(this.Location.y-1,this.Location.x+1));
+		if((this.Location.y-1>-1 & this.Location.y-1<6)&
+				(this.Location.x-1>-1 & this.Location.x-1<6))
+			m.add(new Location(this.Location.y-1,this.Location.x-1));
+		if((this.Location.y+1>-1 & this.Location.y+1<6)&
+				(this.Location.x-1>-1 & this.Location.x-1<6))
+			m.add(new Location(this.Location.y+1,this.Location.x+1));
+return m;	
+	}
+	
 }
