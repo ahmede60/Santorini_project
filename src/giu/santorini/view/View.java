81040f274a8c211f3fc6002c95f730d7c2ec02c1
Mohamed Labeb <mohamed_labeb@icloud.com>
2023-01-13 16:46:43 +0100

    got user from input code

--- src/giu/santorini/view/View.java
+++ src/giu/santorini/view/View.java
@@ -1,10 +1,22 @@
 package giu.santorini.view;
 
-import javax.swing.JFrame;
+import java.util.Scanner;
 
+import javax.swing.JFrame;
 public class View extends JFrame{
 	public View(){
 		
 	}
-
+	public class ScannerClassInput{
+		
+	
+		
+		public void main(String args[])
+	    {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter a String");
+	        String str = sc.nextLine();
+	        System.out.println("The input String is: " +str);
+	    }
+	}
 }
