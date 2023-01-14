e1597dae13c0d0e5454b9ee1f5a5332d32d75faa
Ahmed Alshafie <you@example.com>
2023-01-14 23:40:19 +0300

    made the frames and panels

--- src/giu/santorini/view/View.java
+++ src/giu/santorini/view/View.java
@@ -4,43 +4,62 @@ import giu.santorini.Board;
 import giu.santorini.players.Player;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+@SuppressWarnings("serial")
 public class View extends JFrame{
+	JPanel board;
+	JPanel messages;
 	public View(){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the first player's name");
-        String name1 = sc.nextLine();
-        System.out.println("Enter the second player's name");
-        String name2 = sc.nextLine();
-        boolean correct = true;
+		JFrame input = new JFrame();
+        input.setVisible(true);
+        input.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        input.setSize(500,200);
+        JPanel inputpanel = new JPanel(new GridLayout(2,2));
+        inputpanel.setSize(500,500);
+        inputpanel.setVisible(true);
+        input.add(inputpanel);
+        JTextField name1field = new JTextField("Player1 name");
+        JTextField name2field = new JTextField("Player2 name");
+        JTextField type1field = new JTextField("Player1 type(1 for cube,2 for pyramid)");
+        JTextField type2field = new JTextField("Player2 type(1 for cube,2 for pyramid)");
+        inputpanel.add(name1field);
+        inputpanel.add(name2field);
+        inputpanel.add(type1field);
+        inputpanel.add(type2field);
+        input.revalidate();
+        input.repaint();
+        String name1 = "";
+        String name2 = "";
         String type1 = "";
+        String type2 = "";
+        Player Player1;
+        Player Player2;
+        boolean correct = true;
         while(correct){
-        	System.out.println("Player1: type 1 for cube and type 2 for pyramid");
-        	type1 = sc.nextLine();
+        	type1 = type1field.getText();
+        	name1 = name1field.getText();
         	if ((type1.equals("1"))|(type1.equals("2")))
-        		correct = false;		
+        		correct = false;
         }
         correct = true;
-        String type2 = "";
         while(correct){
-        	System.out.println("Player2: type 1 for cube and type 2 for pyramid");
-        	type2 = sc.nextLine();
+        	type2 = type2field.getText();
+        	name2 = name2field.getText();
         	if ((type2.equals("1"))|(type2.equals("2")))
-        		correct = false;	
+        		correct = false;
         }
-        sc.close();
-        Player Player1;
         if (type1.equals("1")){
         	Player1 = new Player(name1,1);
         }
         else{
         	Player1 = new Player(name1,2);
         }
-        Player Player2;
         if (type2.equals("1")){
         	Player2 = new Player(name2,1);
         }
@@ -48,13 +67,26 @@ public class View extends JFrame{
         	Player2 = new Player(name2,2);
         }
         Board game = new Board(Player1,Player2);
+        input.dispose();
         JFrame window = new JFrame();
         window.setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        window.setSize(1500,1000);
         JPanel main = new JPanel(new BorderLayout());
-        JPanel board = new JPanel(new GridLayout(game.SIDE,game.SIDE));
+        main.setSize(1500,1000);
+        main.setVisible(true);
+        main.setBackground(Color.black);
+        window.add(main);
+        this.board = new JPanel(new GridLayout(game.SIDE,game.SIDE));
+        this.board.setPreferredSize(new Dimension(1000,1000));
+        this.board.setBackground(Color.black);
+        this.board.setVisible(true);
         main.add(board, BorderLayout.CENTER);
-        
+        this.messages = new JPanel();
+        this.messages.setPreferredSize(new Dimension(500,1000));
+        this.messages.setBackground(Color.white);
+        this.messages.setVisible(true);
+        main.add(messages, BorderLayout.EAST);
 	}
 	public static void main(String[] args){
 		new View();
